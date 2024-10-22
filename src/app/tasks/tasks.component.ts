f2701c9da4f3c99394deda2c032f603c20b03cb7
dragan.manikj <draganmanikj2000@gmail.com>
2024-10-22 12:59:24 +0200

    add task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
-
+import { NewTaskComponent } from './new-task/new-task.component';
+import { NewTaskData } from './task/task.model';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({required: true}) userId! : string; 
   @Input() name?: string;
+  isAddingTask = false;
+
   tasks = [
     {
       id: 't1',
@@ -41,5 +44,24 @@ export class TasksComponent {
 
   onCompleteTask(id: string){
     this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskDate: NewTaskData){
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskDate.title,
+      summary: taskDate.summary,
+      dueDate: taskDate.date
+    })
+    this.isAddingTask = false;
   }
 }
